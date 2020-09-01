60c2aac695cd666e944cbea5ab6e2cf7cd4ab81a
dnwdark <Artec40@gmail.com>
2020-09-01 21:34:51 +0500

    Принято решение хранить все изображения в public. В базе данных будут храниться ссылки в данную папку '/imagesDirectory/image'

--- my-app/src/Components/Project/ProjectDescription/ProjectDescription.jsx
+++ my-app/src/Components/Project/ProjectDescription/ProjectDescription.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import s from './ProjectDescription.module.css'
-//todo ссылка на картинку из паблик не работает, нужно пофиксить.
 
 const ProjectDescription = ({project}) => {
     return <div className={s.ProjectDescription}>
