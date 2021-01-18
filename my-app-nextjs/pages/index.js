57dafc95f611452496714ca9543d5c731870c9b5
dnwdark <Artec40@gmail.com>
2021-01-18 22:52:00 +0500

    Отображение данных после загрузки с сервера в исходном коде страницы.

--- my-app-nextjs/pages/index.js
+++ my-app-nextjs/pages/index.js
@@ -1,7 +1,36 @@
 import AboutUsContainer from '../components/AboutUs/AboutUsContainer'
 import Head from 'next/head'
+import { useState, useEffect } from 'react'
+import { aboutUsAPI } from '../api'
 
-export default function Index() {
+export default function Index({props: serverData}) {
+
+    const [data, setData] = useState(serverData)
+
+    useEffect(() => {
+        async function load() {
+            const projects = await aboutUsAPI.getProjects()
+            const employees = await aboutUsAPI.getEmployees()
+            const articles = await aboutUsAPI.getArticles()
+            const data = {
+                projects: projects.data,
+                employees: employees.data,
+                articles: articles.data
+            }
+            setData(data)
+        }
+
+        if (!serverData) {
+            load()
+        }
+    }, [])
+
+    if (!data) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+
     return (
         <>
             <Head>
@@ -13,4 +42,17 @@ export default function Index() {
             <AboutUsContainer/>
         </>
     )
+}
+
+export async function getStaticProps() {
+    const projects = await aboutUsAPI.getProjects()
+    const employees = await aboutUsAPI.getEmployees()
+    const articles = await aboutUsAPI.getArticles()
+    return {
+        props: {
+                projects: projects.data,
+                employees: employees.data,
+                articles: articles.data
+        }
+    }
 }
